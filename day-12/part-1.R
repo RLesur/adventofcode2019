9a57e75f217d4b6da6bc9bc4053502ef70ad9c60
Romain Lesur <romain.lesur@gmail.com>
2019-12-12 08:13:22 +0100

    start day 12

---
+++ day-12/part-1.R
@@ -0,0 +1,42 @@
+Moon <- R6::R6Class(
+  "Moon",
+  private = list(
+    pos = c(),
+    val = c(x = 0, y = 0, z = 0)
+  ),
+  public = list(
+    initialize = function(x, y ,z) {
+      private$pos <- c(x = x, y = y, z = 0)
+    },
+    potential_energy = function() {
+      sum(abs(private$pos))
+    },
+    kinetic_energy = function() {
+      sum(abs(private$vel))
+    },
+    total_energy = function() {
+      self$potential_energy() + self$kinetic_energy()
+    }
+  )
+)
+
+System <- R6::R6Class(
+  "System",
+  private = list(
+    moons = list()
+  ),
+  public = list(
+    initialize = function(...) {
+      moons <- list(...)
+      lapply(moons, function(x) stopifnot("Moon" %in% class(x)))
+      private$moons <- moons
+    }
+  )
+)
+
+# test
+moon1 <- Moon$new(x=-1, y=0, z=2)
+moon2 <- Moon$new(x=2, y=-10, z=-7)
+moon3 <- Moon$new(x=4, y=-8, z=8)
+moon4 <- Moon$new(x=3, y=5, z=-1)
+system <- System$new(moon1, moon2, moon3, moon4)
