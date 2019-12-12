22ab4b9575ea1b71da5a09c0f24019a55bea26f7
Romain Lesur <romain.lesur@gmail.com>
2019-12-12 09:20:46 +0100

    intermediate step

--- day-12/part-1.R
+++ day-12/part-1.R
@@ -7,6 +7,24 @@ Moon <- R6::R6Class(
   public = list(
     initialize = function(x, y ,z) {
       private$pos <- c(x = x, y = y, z = z)
+    },
+    print = function() {
+      cat("pos=<", paste(c("x", "y", "z"), private$pos, sep = "=", collapse = ", "), ">, ",
+          "vel=<", paste(c("x", "y", "z"), private$vel, sep = "=", collapse = ", "), ">\n",
+          sep = "")
+    },
+    get_position = function() {
+      private$pos
+    },
+    get_velocity = function() {
+      private$vel
+    },
+    set_velocity = function(vel) {
+      stopifnot(identical(names(vel), c("x", "y", "z")))
+      private$vel <- vel
+    },
+    apply_velocity = function() {
+      private$pos <- private$pos + private$vel
     }
   ),
   active = list(
@@ -32,6 +50,23 @@ System <- R6::R6Class(
       moons <- list(...)
       lapply(moons, function(x) stopifnot("Moon" %in% class(x)))
       private$moons <- moons
+    },
+    print = function() {
+      lapply(private$moons, function(x) x$print())
+    },
+    get_moons = function() {
+      private$moons
+    },
+    apply_gravity = function() {
+      combs <- gtools::combinations(length(private$moons), 2)
+      for (i in seq.int(1, length.out = nrow(combs))) {
+        indexes <- combs[i, ]
+        moonA <- private$moons[[indexes[1]]]
+        moonB <- private$moons[[indexes[2]]]
+        posA <- moonA$get_position()
+        posB <- moonB$get_position()
+        delta_velA <- ifelse(posA > posB, -1, ifelse(posA < posB, 1, 0))
+      }
     }
   )
 )
@@ -42,3 +77,4 @@ moon2 <- Moon$new(x=2, y=-10, z=-7)
 moon3 <- Moon$new(x=4, y=-8, z=8)
 moon4 <- Moon$new(x=3, y=5, z=-1)
 system <- System$new(moon1, moon2, moon3, moon4)
+moons <- system$get_moons()
